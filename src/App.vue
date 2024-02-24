1b3b61d4a272d09dfc892fa8fe59270b2b5d33e5
karabass92 <shatveryank@gmail.com>
2024-02-24 22:24:17 +0600

    кликабельные ссылки в компоненте TheHeader

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,25 @@ import TheTimeline from './pages/TheTimeline.vue'
 
 import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from './consts/navigation'
 
-
 const currentPage = ref<string>(normalizePageHash())
 
-function normalizePageHash() {
+function normalizePageHash(): string {
   const hash = window.location.hash.slice(1)
   if([PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE].includes(hash)) return hash
   window.location.hash = PAGE_TIMELINE
   return PAGE_TIMELINE
 }
+
+function goTo(page: string): void { currentPage.value = page }
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen justify-between">
-    <TheHeader />
+
+    <TheHeader 
+      @go-to-timeline="goTo(PAGE_TIMELINE)" 
+      @go-to-progress="goTo(PAGE_PROGRESS)"
+    />
 
     <main class="flex flex-grow flex-col">
       <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
@@ -31,6 +36,10 @@ function normalizePageHash() {
       <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
     </main>
 
-    <TheNavbar :current-page="currentPage" @navigate="currentPage = $event" />
+    <TheNavbar 
+      :current-page="currentPage" 
+      @navigate="goTo($event)" 
+    />
+
   </div>
 </template>
--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { PAGE_TIMELINE } from '@/consts/navigation'
+</script>
+
+
 <template>
-    <a href="#">
+    <a :href="`#${PAGE_TIMELINE}`">
         <img src="../assets/img/logo.png" alt="Logo" class="h-9">
     </a>
 </template>
