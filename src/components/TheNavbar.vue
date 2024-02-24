2d4532abc7205461a300d8ee7846802162f2cd86
karabass92 <shatveryank@gmail.com>
2024-02-24 22:52:41 +0600

    разметка временной шкалы

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -8,7 +8,7 @@ const emit = defineEmits(['navigate'])
 </script>
 
 <template>
-    <nav class="stiky bottom-0 z-10 bg-white">
+    <nav class="sticky bottom-0 z-10 bg-white">
         <ul class="flex items-center justify-around border-t">
           <NavItem  
             v-for="(icon, page) in navItems" 
