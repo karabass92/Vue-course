a2c5bcdf26e44867d1bd82163e6b8d4a2928c6d3
karabass92 <shatveryank@gmail.com>
2024-02-24 19:24:12 +0600

    декомпозиция компонентов

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,16 @@
 <script setup lang="ts">
-import { CheckCircleIcon } from '@heroicons/vue/24/solid'
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
-
-const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
-}
+import TheHeader from './components/TheHeader.vue'
+import TheNavbar from './components/TheNavbar.vue'
 </script>
 
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <a href="#">
-      <img src="./assets/img/logo.png" alt="Logo" class="h-9">
-    </a>
-    <a href="#" class="text-sm">
-      <div v-if="true" class="flex items-center gap-1">
-        Day complete!
-        <CheckCircleIcon class="h-7 text-green-500" />
-      </div>
-      <div v-else class="flex items-center gap-1">
-        Progress: 
-        <span class="font-mono">20%</span>
-        <div class="h-3 w-3 rounded-full bg-red-500"></div>
-      </div>
-    </a>
-  </header>
+  <div class="flex flex-col min-h-screen justify-between">
+    <TheHeader />
 
-  <main class="flex flex-grow flex-col">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae asperiores voluptate, nesciunt repellendus vel placeat sunt maxime? Non reprehenderit similique aliquid expedita voluptas hic, eum adipisci ipsa. Eius, eum necessitatibus!
-  </main>
+    <main class="flex flex-grow flex-col">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae asperiores voluptate, nesciunt repellendus vel placeat sunt maxime? Non reprehenderit similique aliquid expedita voluptas hic, eum adipisci ipsa. Eius, eum necessitatibus!
+    </main>
 
-  <nav class="stiky bottom-0 z-10 bg-white">
-    <ul class="flex items-center justify-around border-t">
-      <li v-for="icon, page in navItems" :key="page" class="flex-1">
-        <a v-bind:href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
-          <component v-bind:is="icon"  class="h-6 w-6" /> {{ page }}
-        </a>
-      </li>
-    </ul>
-  </nav>
-
+    <TheNavbar />
+  </div>
 </template>
-
-<style scoped></style>
