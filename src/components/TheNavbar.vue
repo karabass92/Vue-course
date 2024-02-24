7e2c0104134354c2f2d04fe5d8a88200eb9e3ca8
karabass92 <shatveryank@gmail.com>
2024-02-24 21:13:12 +0600

    нормализация хеша страницы

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import NavItem from './NavItem.vue'
+import { ref } from 'vue'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../consts/navigation'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItem from './NavItem.vue'
+
 
 const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
 }
 
-const currentPage = ref('timeline')
+const currentPage = ref<string>(normalizePageHash())
+
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1)
+  if(Object.keys(navItems).includes(hash)) return hash
+  window.location.hash = PAGE_TIMELINE
+  return PAGE_TIMELINE
+}
+
 </script>
 
 <template>
---
+++ src/consts/navigation.ts
@@ -0,0 +1,3 @@
+export const PAGE_TIMELINE: string = 'timeline'
+export const PAGE_ACTIVITIES: string = 'activities'
+export const PAGE_PROGRESS: string = 'progress'
