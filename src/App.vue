f001d76a4da2f90fd053c023bc364359094e6d56
karabass92 <shatveryank@gmail.com>
2024-02-24 15:38:15 +0600

    верстка навигационной панели

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
@@ -19,6 +20,34 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
+
+  <main class="flex flex-grow flex-col">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae asperiores voluptate, nesciunt repellendus vel placeat sunt maxime? Non reprehenderit similique aliquid expedita voluptas hic, eum adipisci ipsa. Eius, eum necessitatibus!
+  </main>
+
+  <nav class="stiky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li class="flex-1">
+        <a href="#timeline" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ClockIcon class="h-6 w-6" />
+          TimeLine
+        </a>
+      </li>
+      <li class="flex-1">
+        <a href="#activities" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ListBulletIcon class="h-6 w-6" />
+          Activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a href="#progress" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ChartBarIcon class="h-6 w-6" />
+          Progress
+        </a>
+      </li>
+    </ul>
+  </nav>
+
 </template>
 
 <style scoped></style>
